864892d30f8693880c508aa3f743306b1015749e
WonHyun Choi <coy4728@naver.com>
2024-07-11 22:19:12 +0900

    etc: temp commit for mac book work

--- lib/widgets/posting/posting_screen.dart
+++ lib/widgets/posting/posting_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mission_diary/global/gaps.dart';
 import 'package:mission_diary/global/sizes.dart';
+import 'package:mission_diary/util/date_util.dart';
 import 'package:mission_diary/widgets/common/constrainted_body.dart';
+import 'package:mission_diary/widgets/common/rounded_button.dart';
 import 'package:mission_diary/widgets/posting/widgets/emoji_slider.dart';
 
 class PostingScreen extends ConsumerStatefulWidget {
@@ -14,32 +17,61 @@ class PostingScreen extends ConsumerStatefulWidget {
 }
 
 class _PostingScreenState extends ConsumerState<PostingScreen> {
+  int score = 75;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ConstraintedBody(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () => context.pop(),
-                  icon: const FaIcon(
-                    FontAwesomeIcons.chevronDown,
-                    size: Sizes.size20,
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () => context.pop(),
+                    icon: const FaIcon(
+                      FontAwesomeIcons.chevronDown,
+                      size: Sizes.size20,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const Text("Mission Score"),
-            Text(
-              "72",
-              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                    fontWeight: FontWeight.w600,
-                  ),
-            ),
-            const EmojiSlider(score: 72),
-          ],
+                ],
+              ),
+              Text(
+                getYYYYMMDD(DateTime.now()),
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+              ),
+              Gaps.v32,
+              const Text("Today your Score is"),
+              Gaps.v10,
+              Text(
+                "$score",
+                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+              ),
+              Gaps.v10,
+              const Text("Great Job !"),
+              Gaps.v32,
+              Text(
+                "How satisfied are you today?",
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+              ),
+              EmojiSlider(score: score),
+              Gaps.v32,
+              RoundedButton(
+                onTap: () => {},
+                text: "Send to Headquarters",
+                fontColor: Colors.white,
+                backgroundColor: Theme.of(context).colorScheme.primary,
+              ),
+            ],
+          ),
         ),
       ),
     );
