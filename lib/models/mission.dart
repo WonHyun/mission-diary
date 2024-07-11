674ee9369a598b9f880c6fbc4ee2ec1bccbf987b
WonHyun <coy4728@naver.com>
2024-07-11 16:27:44 +0900

    feat: implement mission model

---
+++ lib/models/mission.dart
@@ -0,0 +1,27 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:mission_diary/global/enum.dart';
+
+part 'mission.freezed.dart';
+part 'mission.g.dart';
+
+@freezed
+class Mission with _$Mission {
+  const factory Mission({
+    required String missionId,
+    required String userId,
+    required DateTime createdAt,
+    required MissionType type,
+    required bool isCompleted,
+    required bool isNeedPhoto,
+    required MissionFrequency frequency,
+    required String title,
+    required String description,
+    required DateTime startAt,
+    required DateTime endAt,
+    required Duration duration,
+    required List<String> tag,
+  }) = _Mission;
+
+  factory Mission.fromJson(Map<String, dynamic> json) =>
+      _$MissionFromJson(json);
+}
