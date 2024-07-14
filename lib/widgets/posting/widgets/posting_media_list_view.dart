73998d06808343e0133e06b8af50c90c774dc368
WonHyun <coy4728@naver.com>
2024-07-14 18:37:05 +0900

    fix: adjust height and add text message when empty media

--- lib/widgets/posting/widgets/posting_media_list_view.dart
+++ lib/widgets/posting/widgets/posting_media_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mission_diary/global/gaps.dart';
+import 'package:mission_diary/global/sizes.dart';
 import 'package:mission_diary/widgets/common/check_circle.dart';
 import 'package:mission_diary/widgets/common/safe_image.dart';
 import 'package:mission_diary/widgets/posting/view_model/media_view_model.dart';
@@ -8,12 +9,10 @@ import 'package:mission_diary/widgets/posting/view_model/media_view_model.dart';
 class PostingMediaListView extends ConsumerWidget {
   const PostingMediaListView({
     super.key,
-    this.height = 150,
-    this.width = 200,
+    this.height = 160,
   });
 
   final double height;
-  final double width;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -21,38 +20,51 @@ class PostingMediaListView extends ConsumerWidget {
           data: (medias) {
             return SizedBox(
               height: height,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                separatorBuilder: (context, index) => Gaps.h10,
-                itemCount: medias.length,
-                itemBuilder: (context, index) {
-                  final media = medias[index];
-                  return GestureDetector(
-                    onTap: () =>
-                        ref.read(mediaProvider.notifier).selectMedia(media),
-                    child: Stack(
-                      children: [
-                        Container(
-                          width: width,
-                          clipBehavior: Clip.hardEdge,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: SafeImage(
-                            path: media.path,
-                            pathType: media.mediaPathType,
-                          ),
+              child: medias.isEmpty
+                  ? const Center(
+                      child: Text(
+                        "Have no Photos, Let's add proof photos",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: Sizes.size16,
                         ),
-                        Positioned(
-                          top: 8,
-                          left: 8,
-                          child: CheckCircle(isChecked: media.isSelected),
-                        ),
-                      ],
+                      ),
+                    )
+                  : ListView.separated(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      separatorBuilder: (context, index) => Gaps.h10,
+                      itemCount: medias.length,
+                      itemBuilder: (context, index) {
+                        final media = medias[index];
+                        return GestureDetector(
+                          onTap: () => ref
+                              .read(mediaProvider.notifier)
+                              .selectMedia(media),
+                          child: Stack(
+                            children: [
+                              Container(
+                                clipBehavior: Clip.hardEdge,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: SafeImage(
+                                  height: height,
+                                  path: media.path,
+                                  pathType: media.mediaPathType,
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              ),
+                              Positioned(
+                                top: 8,
+                                left: 8,
+                                child: CheckCircle(isChecked: media.isSelected),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
             );
           },
           error: (err, stack) => Center(
